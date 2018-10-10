bfbe8fe156efd7341e0283d4e98fa8839efe9dc8
avaaris <avaaris@gmail.com>
2018-10-10 20:05:59 +0200

    6ig

--- 4es_Dia_feladatok/src/elso.java
+++ 4es_Dia_feladatok/src/elso.java
@@ -6,38 +6,38 @@ public class elso {
 		for (int i = 0; i < args.length; i++) {
 			System.out.println(args[i]);
 		}
-		
+
 		// második f
 		for (int i = 0; i <= 20; i++) {
 			if (i % 2 == 0) {
 				System.out.println(i);
 			}
 		}
-		
+
 		// 3. f
 		int a = Integer.parseInt(args[0]);
 		int b = Integer.parseInt(args[1]);
-		
+
 		for (int i = a; i <= b; i++) {
 			if (i % 3 == 0) {
 				System.out.println(i);
 			}
 		}
-		
+
 		// 4. f
 		int c = 0;
 		for (int i = 1; i <= a; i++) {
-			
+
 			if (a % i == 0) {
-				System.out.println(i+ " osztója a "+a+"-nak");
+				System.out.println(i + " osztója a " + a + "-nak");
 				c++;
 			}
-			
+
 		}
 		if (c == 2) {
 			System.out.println("A szám prímszám");
 		}
-		
+
 		// 5.f
 		String s = args[2];
 		int szorzo = Integer.parseInt(args[3]);
@@ -47,9 +47,18 @@ public class elso {
 			}
 			System.out.println("\n");
 		}
-		
+
 		// 6.f
-		
+		int sum = 0;
+		for (int i = 1; i < a; i++) {
+			if (a % i == 0) {
+				sum += i;
+			}
+		}
+		if (a == sum) {
+			System.out.println("A szám tökéletes");
+		} else
+			System.out.println("A szám nem tökéletes");
 	}
 
 }
