130119f9d3cf1c3400d97bbc6e8d9c04f0e4cf26
avaaris <avaaris@gmail.com>
2018-10-10 18:21:10 +0200

    1-3

--- 4es_Dia_feladatok/src/elso.java
+++ 4es_Dia_feladatok/src/elso.java
@@ -2,7 +2,24 @@
 public class elso {
 
 	public static void main(String[] args) {
+		for (int i = 0; i < args.length; i++) {
+			System.out.println(args[i]);
+		}
 		
+		for (int i = 0; i <= 20; i++) {
+			if (i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+		
+		int a = Integer.parseInt(args[0]);
+		int b = Integer.parseInt(args[1]);
+		
+		for (int i = a; i <= b; i++) {
+			if (i % 3 == 0) {
+				System.out.println(i);
+			}
+		}
 	}
 
 }
